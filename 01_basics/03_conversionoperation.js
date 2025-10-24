62d125dda7b1cf6f24a98bc2715a70582a088f99
Harshita garg <Harshitag2518@gmail.com>
2025-10-24 21:00:54 +0000

    datatypes done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,48 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33"=> 33
+// "harshita12" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = " "
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// operationss
+
+ let value = 3
+ let negValue = -value
+//  console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Harshita"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ 01_basics/04_comparisionsinjs.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2==1);
+//always verify the datatypes because its give wrong output when the datatypes are not same.
+
+// work difference in equality check == and comparsions > < >= <= example:
+
+console.log(null>0);//false
+console.log(null == 0);//false
+console.log(null>=0);//true because null is convert into number and treated as 0.
+
+//this is only happen in javascript and in typescript it will give error because typescript is a fully ruled language.
+//=== in this it checks both value and datatype
+
+
+//two types: primitive and non primitive(reference)
+//primitive: number, string, boolean, null, undefined, symbol, number, bigInt
+//non primitive: object, array, function
