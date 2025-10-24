403376d4d95c4312b2429a7e0ae2590b41b6561e
Harshita garg <Harshitag2518@gmail.com>
2025-10-24 11:15:15 +0000

    datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 143232
+let accountEmail = "harshita@gmail.com"
+var accountPassword = "12343"
+accountCity = "jaipur"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using node.js, not browser
+
+console.log(3+3) //code readability should be high
+// documentation in tc39.ecma and mdn
+
+console.log("harshita")
+
+// number => 2 to power 53
+// bigint for big number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not defined
+// symbol =>unique
+
+// object
+// null ek object h 
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
