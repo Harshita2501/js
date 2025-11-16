f327c22e1a994b052a3673527e3d31c9f28e8004
Harshita garg <Harshitag2518@gmail.com>
2025-11-16 11:08:19 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name ="Harshita"
+const repocount = 40
+// console.log(name + repocount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// string interpolation
+
+const gameName = new String('harshita')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
