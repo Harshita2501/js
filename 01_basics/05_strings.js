d1dbe7fd6565649c2578e134054fdf95de18e66a
Harshita garg <Harshitag2518@gmail.com>
2025-12-22 15:45:04 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,3 +9,20 @@ const gameName = new String('harshita')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+console.log(gameName.toLowerCase());
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+console.log(gameName.slice(-8,2));
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('hitesh','harshita'));
+
