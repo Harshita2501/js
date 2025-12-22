b8b29395782cb3b669ca1e83c31fbeb564c4f29c
Harshita garg <Harshitag2518@gmail.com>
2025-12-22 15:46:57 +0000

    Numbers, Maths

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,24 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// const otherNumber = 123.456
+
+// console.log(otherNumber.toPrecision(3));
+
+
+// +++++++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.sqrt(4));
+console.log(Math.pow(4,3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random()*(max-min+1))+min));
