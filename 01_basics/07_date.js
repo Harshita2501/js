69d1444875be3690b7cdb6b96b4e65d8e44b9e11
Harshita garg <Harshitag2518@gmail.com>
2025-12-27 10:01:40 +0000

    Dates

---
+++ 01_basics/07_date.js
@@ -0,0 +1,23 @@
+// Dates
+// Date is a object in JavaScript used to work with dates and times.
+
+// let myDate = new Date(); // Current date and time
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2025, 11, 27);
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getHours());
+
+
+
