bd1f065c3b26cbbb545cf911056f5027010e6097
Harshita garg <Harshitag2518@gmail.com>
2026-02-12 06:45:24 +0000

    execution process

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -36,3 +36,38 @@ const addtwo = (num1, num2) => ({username: "harshita"})
 
 
 console.log(addtwo(3,4));
+
+// javascript execution context 
+// javascript jo h vo code line by line run kar thi h  
+// global execution context (this ka andar sara memory hoti h iss ki )
+// function execution context 
+// eval execution context (ya properties hi hota h global ki )
+
+// two phases
+// memory Creation Phase
+// execution Phase
+
+// example let val1 = 10
+//  let val2 = 5
+// function addNum(num1, num2){
+// let total = num1+ num2
+// return total
+// }
+// let result1 = addNum(val1, val2)
+// let result2 = addNum(10,3)
+
+// function always create new execution environment in execution phase
+
+// kaisa execution hota h uss ka process 
+// global execution -> memory phase -> execution phase
+// global ki values sari this ma jati h ->
+// memory phase ma val1-undefined, val2-undefined, addNum-defination, result1-undefined, result2-undefined->
+// execution Phase ma val1-10, val2-5,addNum aapna naya execution environment bana gya(new variable environment + Execution thread(iss ma frr sa memory phase aur execution phase bana gya kyuki global phase bss ek baar hi aata h))
+// 
+// function new space bana tha h jab uss ka kaam ho jata h vo ussa delete kar deta h means function jitni baar call ho gya vo utni baar new environment bana gya aapna
+// ya line7 ka liya h  addNum-> memory phase-> val1-undefined, val2-undefined, total-undefined
+// execution phase-> num1-10, num2-5, total-15
+//  ya value upar execution phase ma jaa ka result1- 15 ho jaygaa
+// ya sara kaam frr sa ho gya then result2- 13 ho jaygaa
+
+// call stack means values kon si values hum bula rahaa h execution ka liya
