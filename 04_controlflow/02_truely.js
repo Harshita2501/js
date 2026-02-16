6cc0136b36fa2bb847ef0585df944f5917d6a192
Harshita garg <Harshitag2518@gmail.com>
2026-02-16 05:30:17 +0000

    truthy values

---
+++ 04_controlflow/02_truely.js
@@ -0,0 +1,18 @@
+// flasy values
+//  false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", "false", " ", [], {}, function(){}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10  //iss ka output aaya gya 10 iss ka kaam yahi hota h 
+
+console.log(val1);  //output is 5
+
+
+// terniary operator
+
+// condition ? true : false
